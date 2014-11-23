182e24ffc8892cc719c30b30eb80df28e9990f88
radams <ra.mi.adams@gmail.com>
2014-11-22 23:52:48 -0500

    corrected output for tidy dataset

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,9 @@
 
 # requires library(ddplyr)
 # install package if necessary
+if("plyr" %in% rownames(installed.packages()) == FALSE) {install.packages("plyr")};library(plyr)
 if("dplyr" %in% rownames(installed.packages()) == FALSE) {install.packages("dplyr")};library(dplyr)
+if("reshape2" %in% rownames(installed.packages()) == FALSE) {install.packages("reshape2")};library(reshape2)
 
 ############################
 # Overview of this code:
@@ -11,7 +13,8 @@ if("dplyr" %in% rownames(installed.packages()) == FALSE) {install.packages("dply
 # Extracts only the measurements on the mean and standard deviation for each measurement. 
 # Uses descriptive activity names to name the activities in the data set
 # Appropriately labels the data set with descriptive variable names. 
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# From the data set in step 4, creates a second, independent tidy data set with the 
+#   average of each variable (measurement) for each activity and each subject.
 
 # Load data
 data_dir <- "UCI\ HAR\ Dataset"
@@ -46,14 +49,14 @@ x_test_data <- read.table(x_test_file, header=F)
 names(x_test_data) <- feature_list
 
 # add activities to test data and annotate
-# the row indicies in the x and y files are used to map measurements (x) with activities (y)
-# annotation did not use 'merge', which would rearrange the original order of the activity ids
+# the row indicies in the x and y files map measurements (x) with activities (y)
+# annotation does not use 'merge()', which would rearrange the original order of the activity ids
+# annotation replaces "ActivityID" with "ActivityName"
 y_test_data <- read.table(y_test_file, header=F)
 names(y_test_data) <- "ActivityID"
-ordered_activity_ids = as.vector(t(unique(arrange(y_test_data, ActivityID)["ActivityID"])))
-y_test_data$ActivityName = mapvalues(as.vector(t(y_test_data["ActivityID"])), from=ordered_activity_ids, to=ordered_activity_names)
 test_data <- cbind(y_test_data, x_test_data)
-test_data <- test_data[2:ncol(test_data)]
+test_data$ActivityID <- factor(test_data$ActivityID, labels=ordered_activity_names)
+names(test_data)[1] = "ActivityName"
 rm(x_test_data)
 rm(y_test_data)
 
@@ -62,6 +65,10 @@ subject_test_data <- read.table(subject_test_file, header=F, sep=" ")
 names(subject_test_data) <- c("SubjectID")
 test_data <- cbind(subject_test_data, test_data)
 rm(subject_test_data)
+
+# tidy test data
+# melt data frame to have 4 columns: SubjectID, ActivityName, MeasurementName, MeasurementValue
+test_data <- melt(test_data, id.vars=c("SubjectID", "ActivityName"), variable.name="MeasurementName", value.name="MeasurementValue")
 
 # set up names of train files
 x_train_file <- paste(data_dir, "X_train.txt", sep="/train/")
@@ -75,12 +82,12 @@ names(x_train_data) <- feature_list
 # add activities to train data and annotate
 # the row indicies in the x and y files are used to map measurements (x) with activities (y)
 # annotation did not use 'merge', which would rearrange the original order of the activity ids
+# annotation replaces "ActivityID" with "ActivityName"
 y_train_data <- read.table(y_train_file, header=F)
 names(y_train_data) <- "ActivityID"
-ordered_activity_ids = as.vector(t(unique(arrange(y_train_data, ActivityID)["ActivityID"])))
-y_train_data$ActivityName = mapvalues(as.vector(t(y_train_data["ActivityID"])), from=ordered_activity_ids, to=ordered_activity_names)
 train_data <- cbind(y_train_data, x_train_data)
-train_data <- train_data[,2:ncol(train_data)]
+train_data$ActivityID <- factor(train_data$ActivityID, labels=ordered_activity_names)
+names(train_data)[1] = "ActivityName"
 rm(x_train_data)
 rm(y_train_data)
 
@@ -90,18 +97,26 @@ names(subject_train_data) <- c("SubjectID")
 train_data <- cbind(subject_train_data, train_data)
 rm(subject_train_data)
 
+# tidy train data
+# melt data frame to have 4 columns: SubjectID, ActivityName, MeasurementName, MeasurementValue
+train_data <- melt(train_data, id.vars=c("SubjectID", "ActivityName"), variable.name="MeasurementName", value.name="MeasurementValue")
+
 # merge the training and the test sets to create one data set
-tidy_data <- rbind(test_data, train_data)
+all_data <- rbind(test_data, train_data)
 rm(train_data)
 rm(test_data)
 
 # extract only the measurements on the mean and standard deviation
-tidy_data <- cbind(tidy_data[,c(1:2)], tidy_data[, grepl("-mean|std", names(tidy_data), ignore.case=T)])
+tidy_data <- all_data[grepl("-mean|std", all_data$MeasurementName, ignore.case=T),]
 
 # create a second, independent tidy data set with the average of each variable
 #   for each activity and each subject
 second_tidy_data <- tbl_df(tidy_data) 
 by_subject_activity <- second_tidy_data %>% 
-    group_by(SubjectID, ActivityName) %>%
-    summarise_each(funs(mean))
+    group_by(SubjectID, ActivityName, MeasurementName) %>%
+    summarise_each(funs(mean),matches("Value"))
+names(by_subject_activity)[4] = "MeasurementAverage"
+
+# write second tidy data set to txt file
 write.table(by_subject_activity, file="subject_activity_averages.txt", row.name=FALSE, quote=FALSE, sep="\t")
+
